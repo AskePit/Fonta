c710a5347aba22fd5790734ae7e158557753121c
Nikolay Shalakin <nikolay.shalakin@astrosoft.ru>
2016-12-14 16:25:25 +0100

    truncate all fonts' garbage suffixes

--- resources/known_fonts/process.py
+++ resources/known_fonts/process.py
@@ -1,11 +1,20 @@
 import glob
 
+to_trunc = ['std', 'pro', 'normal', 'regular', 'italic', 'bold', 'font', 'demo', 'condenced']
+
+def trunc(s):
+    for sub in to_trunc:
+        if s.endswith(' ' + sub):
+            s = s[:len(s) - (len(sub) + 1)]
+    return s
+
 def format_file(filename):
     fonts = open(filename, 'rb').readlines()
 
-    fonts = list(map(lambda x:x.decode('utf-8').lower(), fonts))
-    fonts = list(map(lambda x:' '.join(x.split()), fonts))
-    fonts = list(set(fonts))
+    fonts[:] = [x.decode('utf-8').lower() for x in fonts]
+    fonts[:] = [' '.join(x.split()) for x in fonts]
+    fonts[:] = [trunc(x) for x in fonts]
+    fonts[:] = list(set(fonts))
     fonts.sort()
 
     out = open(filename, 'wb')
